f22757fdb0f4b6bdc9e5289afe3d1cd07257897d
Rokibul Islam <rokib.hridoy@gmail.com>
2021-04-10 00:34:51 +0600

    login, registration and profile management backend  deployed.

--- code_aqua/urls.py
+++ code_aqua/urls.py
@@ -15,8 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from user import views as user_views
+from django.contrib.auth import views as auth_views
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('home.urls')),
-]
+    path('admin/', admin.site.urls),
+    
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'), name='password_reset'),
+    
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), name='password_reset_done'),
+    
+    path('password_reset-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name='password_reset_confirm'),
+    
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'), name='password_reset_complete'),
+    
+    path('profile/', user_views.profile, name='profile'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+import os
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default_profile.png',  upload_to='profile_pics')
+    institution = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    occupation = models.CharField(max_length=30)
+    phone = models.CharField(max_length=11, blank=True)
+    bio = models.CharField(max_length=200, blank=True)
+    theme = models.CharField(max_length=10, choices=[('Day','Day'), ('Night','Night')], default='Day')
+    
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+
+
+    def save(self):
+        super().save()
+        
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
