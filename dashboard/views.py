ceb6663444eeeb98c1b0f28b6c93f27185cfd262
Rokibul Islam <rokib.hridoy@gmail.com>
2021-06-05 03:22:11 +0600

    req updated

--- dashboard/views.py
+++ dashboard/views.py
@@ -19,28 +19,31 @@ def dashboard(request):
     total_spent = 0
     total_daily_spent = 0
     count = 0
- 
-    context['budget'] = Budget.objects.get(owner=request.user)
-    context['daily'] = DailySpend.objects.filter(owner=request.user)
+    avg = 0
+    try:
+        context['budget'] = Budget.objects.get(owner=request.user)
+        context['daily'] = DailySpend.objects.filter(owner=request.user)
+        
+        for i in context['daily']:
+            total_daily_spent += i.amount_Spent
+            count += 1
+
+        if count > 0:
+            avg = float(total_daily_spent) / count
+        else: 
+            avg = 0
+        total_spent = (total_daily_spent + context['budget'].food_budget +
+                    context['budget'].house_rent + context['budget'].elec_bill + context['budget'].gas_bill +
+                    context['budget'].water_bill + context['budget'].other_bill)
+        context['cur_bal'] = context['budget'].total_budget - total_spent
+    except:
+        context['budget'] = None
+        context['daily'] = None
+
     
-    for i in context['daily']:
-        total_daily_spent += i.amount_Spent
-        count += 1
-
-    if count > 0:
-        avg = float(total_daily_spent) / count
-    else: 
-        avg = 0
-    total_spent = (total_daily_spent + context['budget'].food_budget +
-                context['budget'].house_rent + context['budget'].elec_bill + context['budget'].gas_bill +
-                context['budget'].water_bill + context['budget'].other_bill)
-
-    context['cur_bal'] = context['budget'].total_budget - total_spent
     context['avg'] = avg
     context['count'] = count
     
-
-
     return render(request, 'dashboard/index.html', context)
 
 
